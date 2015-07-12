183e02b073db9c802ac628f28cc6ac11c87b5661
Ignacio Carbajo <icarbajop@gmail.com>
2015-07-12 20:16:04 +0200

    Start with new diagram

--- projects/angular2/shared.js
+++ projects/angular2/shared.js
@@ -1,3 +1,4 @@
 diagrams.shared.set({
-    project: 'Angular 2',
+  project: 'Angular 2',
+  "class-ChangeDetectorRef": "Controls change detection. Allows requesting checks for detectors that rely on observables. It also allows detaching and attaching change detector subtrees.",
 });
