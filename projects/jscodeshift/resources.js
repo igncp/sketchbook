e40d89cd2a726c7298152df2ad2f4032aec51740
Ignacio Carbajo <icarbajop@gmail.com>
2015-10-18 20:03:57 +0100

    Fix diagram name

--- projects/jscodeshift/resources.js
+++ projects/jscodeshift/resources.js
@@ -1,11 +1,12 @@
 var d = diagrams.box.generateDefinition,
-    l = diagrams.box.generateLink;
+  l = diagrams.box.generateLink,
+  s = diagrams.shared.get;
 
 diagrams.box({
-    name: 'Ramda resources',
-    body: [
-        l('Repo', "https://github.com/facebook/jscodeshift"),
-        l('Article', "https://medium.com/@cpojer/effective-javascript-codemods-5a6686bb46fb"),
-        l('Presentation', "https://www.youtube.com/watch?v=d0pOgY8__JM"),
-    ]
+  name: s('project') + 'resources',
+  body: [
+    l('Repo', "https://github.com/facebook/jscodeshift"),
+    l('Article', "https://medium.com/@cpojer/effective-javascript-codemods-5a6686bb46fb"),
+    l('Presentation', "https://www.youtube.com/watch?v=d0pOgY8__JM"),
+  ]
 });
