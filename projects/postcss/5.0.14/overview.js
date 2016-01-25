ba5cad5383d46f6cf8fea840fd50d12ea5d5726f
Ignacio Carbajo <icarbajop@gmail.com>
2016-01-25 21:32:20 +0000

    Few additions to project

--- projects/postcss/5.0.14/overview.js
+++ projects/postcss/5.0.14/overview.js
@@ -5,5 +5,7 @@ var d = diagrams.box.generateDefinition,
 diagrams.box({
   name: s('project') + ' overview',
   body: [
+    "The library is plugin-oriented, and the main part of its benefits will come from external plugins."
+      + " It provides helpers to parse css (including support for source maps). It is written following an object-oriented approach."
   ]
 });
--- projects/postcss/resources.js
+++ projects/postcss/resources.js
@@ -7,5 +7,6 @@ diagrams.box({
   body: [
     l('Repo', "https://github.com/postcss/postcss"),
     l('Docs', "https://github.com/postcss/postcss/tree/5.0.14/docs"),
+    l('Plugins list', "https://github.com/himynameisdave/postcss-plugins"),
   ]
 });
