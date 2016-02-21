96c757495351ff85fa6251e245583b77167f1e68
Ignacio Carbajo <icarbajop@gmail.com>
2016-02-21 19:27:24 +0000

    Add diagram

--- projects/prometheus/0.17.0rc2/src/glossary.js
+++ projects/prometheus/0.17.0rc2/src/glossary.js
@@ -7,6 +7,17 @@ diagrams.box({
   body: [
     c('Pending', 'Concepts with no knowledge about them', [
       c("prometheus", [
+        c("cmd", [
+          c("prometheus", [
+            c("config.go", [
+              "query engine",
+              "sync strategy (e.g. adaptive)",
+            ]),
+            c("main.go", [
+              "fanout", "target manager", "rule manager",
+            ]),
+          ]),
+        ]),
         c("config", [
           c("config.go", [
             "[prometheus] model",
@@ -14,35 +25,52 @@ diagrams.box({
             "ca file",
             "config.xxx",
             "[Twitter] serverset",
-            "honor labels",
             "marshall/unmarshall",
             "refresh interval",
-            "relabel",
-            "rule files",
             "scheme(http)",
             "scrape/evaluation",
             "sd config",
             "serverset",
-            "target address",
-            "label",
             "target groups",
             "tls config",
             "type (srv)",
-          ])
+          ]),
         ]),
       ])
     ]),
-    c('General', []),
+    c('General', [
+      d("prometheus target", "'One application, server or endpoint that Prometheus is scraping.'"),
+      d("prometheus instance", "'Any individually scraped target is called an instance, usually corresponding to a single process. A collection of instances of the same type (replicated for scalability or reliability) is called a job.'"),
+      d("prometheus counter", "'A counter is a cumulative metric that represents a single numerical value that only ever goes up. A counter is typically used to count requests served, tasks completed, errors occurred, etc.'"),
+      d("prometheus gauge", "'A gauge is a metric that represents a single numerical value that can arbitrarily go up and down.'"),
+    ]),
     c('Structured', [
+      c("cmd", [
+        c("prometheus", [
+          c("main.go", [
+            d("reloadable", "'Reloadable things can change their internal state to match a new config and handle failure gracefully.'"),
+          ]),
+        ]),
+        c("promtool", [
+          c("main.go", [
+            d("promtool", "'To quickly check whether a rule file is syntactically correct without starting a Prometheus server, install and run Prometheus's promtool command-line utility tool. Aims: validate configuration files for correctness, validate rule files for correctness'"),
+          ]),
+        ]),
+      ]),
       c("config", [
         c("config.go", [
           d("AirBnB Nerve", "'Nerve is a utility for tracking the status of machines and services. It runs locally on the boxes which make up a distributed system, and reports state information to a distributed key-value store.'"),
           d("Consul datacenter", "'We define a datacenter to be a networking environment that is private, low latency, and high bandwidth. This excludes communication that would traverse the public internet, but for our purposes multiple availability zones within a single EC2 region would be considered part of a single datacenter.'"),
           d("EC2", "'Amazon Elastic Compute Cloud (Amazon EC2) is a web service that provides resizable compute capacity in the cloud. It is designed to make web-scale cloud computing easier for developers.'"),
+          d("honor labels", "'Whether the target's labels have precedence over the base labels assigned by the scraping instance.'"),
           d("Kubernetes kubelet", "'The kubelet is the primary \"node agent\" that runs on each node. The kubelet works in terms of a PodSpec. A PodSpec is a YAML or JSON object that describes a pod. The kubelet takes a set of PodSpecs that are provided through various mechanisms (primarily through the apiserver) and ensures that the containers described in those PodSpecs are running and healthy.'"),
+          d("label", "'Use labels to differentiate the characteristics of the thing that is being measured'"),
           d("Marathon", "By mesosphere. 'A cluster-wide init and control system for services in cgroups or Docker containers'"),
           d("model", "'Prometheus fundamentally stores all data as time series: streams of timestamped values belonging to the same metric and the same set of labeled dimensions. Besides stored time series, Prometheus may generate temporary derived time series as the result of queries.'"),
+          d("relabel", "'Relabeling is a powerful tool to dynamically rewrite the label set of a target before it gets scraped. Multiple relabeling steps can be configured per scrape configuration. They are applied to the label set of each target in order of their appearance in the configuration file.'"),
+          d("rule files", "'Prometheus supports two types of rules which may be configured and then evaluated at regular intervals: recording rules and alerting rules. [...] The rule files can be reloaded at runtime by sending SIGHUP to the Prometheus process. The changes are only applied if all rule files are well-formatted.'"),
           d("SD", "Service Discovery"),
+          d("target address", "'ip of the target'"),
           d("TLS", "'Transport Layer Security (TLS) is a protocol that ensures privacy between communicating applications and their users on the Internet. When a server and client communicate, TLS ensures that no third party may eavesdrop or tamper with any message. TLS is the successor to the Secure Sockets Layer (SSL).'"),
         ])
       ]),
--- projects/prometheus/0.17.0rc2/src/manual-api.js
+++ projects/prometheus/0.17.0rc2/src/manual-api.js
@@ -3,10 +3,185 @@ var d = diagrams.box.generateDefinition,
   s = diagrams.shared.get;
 
 diagrams.box({
-  name: s('project') + ' src manual api',
+  name: 'prometheus manual-api',
   body: [
-  c("config", [
-    
-  ])
+    c("cmd", [
+      c("prometheus", [
+        d("Main() int", "'Main manages the startup and shutdown lifecycle of the entire Prometheus server.'"),
+        c("Reloadable interface", "'Reloadable things can change their internal state to match a new config and handle failure gracefully.'", [
+          "ApplyConfig(*config.Config) bool",
+        ]),
+      ]),
+      c("promtool", [
+        d("CheckConfigCmd(t cli.Term, args ...string) int", "'CheckConfigCmd validates configuration files.'"),
+        d("CheckRulesCmd(t cli.Term, args ...string) int", "'CheckRulesCmd validates rule files.'"),
+        d("VersionCmd(t cli.Term, _ ...string) int", "'VersionCmd prints the binaries version information.'"),
+      ]),
+    ]),
+    d("config"),
+    d("notification"),
+    d("promql"),
+    d("retrieval"),
+    c("retrieval", [
+      d("discovery"),
+      c("discovery", [
+        d("marathon"),
+        d("kubernetes"),
+      ]),
+    ]),
+    d("rules"),
+    d("storage"),
+    c("storage", [
+      d("local"),
+      c("local", [
+        d("codable"),
+        d("index"),
+      ]),
+      d("metric"),
+      d("remote"),
+      c("remote", [
+        d("graphite"),
+        d("influxdb"),
+        d("opentsdb"),
+      ]),
+    ]),
+    d("template"),
+    c("util", [
+      c("cli", [
+        c("type Command struct", "Command represents a single command within an application.", [
+          "Desc string",
+          "Run  func(t Term, args ...string) int",
+        ]),
+        c("type Term interface", "Term handles an application's output.", [
+          "Infof(format string, v ...interface{})",
+          "Errorf(format string, v ...interface{})",
+          "Out(format string)",
+        ]),
+        c("type App struct", "App represents an application that may consist of multiple commands.", [
+          "Name string",
+          "Help func() string",
+          "commands map[string]*Command",
+          d("(app *App) Register(name string, cmd *Command)", "'Register adds a new command to the application.'"),
+          d("(app *App) Run(t Term, args ...string) int", "'Run the application with the given arguments. Output is sent to t.'"),
+        ]),
+        d("BasicHelp(app *App, ts string) func() string", "'BasicHelp returns a function that creates a basic help text for the application with its commands.'"),
+        d("BasicTerm(out, err io.Writer) Term", "'BasicTerm returns a Term writing Infof and Errorf to err and Out to out.'"),
+        d("NewApp(name string) *App", "'NewApp creates a new application with a pre-registered help command.'"),
+      ]),
+      d("flock"),
+      c("httputil", [
+        d("NewClient(rt http.RoundTripper) *http.Client", "NewClient returns a http.Client using the specified http.RoundTripper."),
+        d("NewDeadlineClient(timeout time.Duration, proxyURL *url.URL) *http.Client", "NewDeadlineClient returns a new http.Client which will time out long running requests."),
+        d("NewDeadlineRoundTripper(timeout time.Duration, proxyURL *url.URL) http.RoundTripper", "NewDeadlineRoundTripper returns a new http.RoundTripper which will time out long running requests."),
+        d("NewBearerAuthRoundTripper(bearer string, rt http.RoundTripper) http.RoundTripper", "NewBearerAuthRoundTripper adds the provided bearer token to a request unless the authorization header has already been set."),
+        d("NewBasicAuthRoundTripper(username, password string, rt http.RoundTripper) http.RoundTripper", "NewBasicAuthRoundTripper will apply a BASIC auth authorization header to a request unless it has already been set."),
+        c("type TLSOptions struct", [
+          "InsecureSkipVerify bool",
+          "CAFile             string",
+          "CertFile           string",
+          "KeyFile            string",
+        ]),
+        "NewTLSConfig(opts TLSOptions) (*tls.Config, error)",
+        c("type CompressionHandler struct", "CompressionHandler is a wrapper around http.Handler which adds suitable response compression based on the client's Accept-Encoding headers.", [
+          "Handler http.Handler",
+          d("(c CompressionHandler) ServeHTTP(writer http.ResponseWriter, req *http.Request)", "ServeHTTP adds compression to the original http.Handler's ServeHTTP() method."),
+        ]),
+      ]),
+      c("stats", [
+        c("type Timer struct", "A Timer that can be started and stopped and accumulates the total time it was running (the time between Start() and Stop()).", [
+          "name     fmt.Stringer",
+          "created  time.Time",
+          "start    time.Time",
+          "duration time.Duration",
+          d("(t *Timer) Start() *Timer", "'Start the timer.'"),
+          d("(t *Timer) Stop()", "'Stop the timer.'"),
+          d("(t *Timer) ElapsedTime() time.Duration", "'ElapsedTime returns the time that passed since starting the timer.'"),
+          d("(t *Timer) String() string", "'Return a string representation of the Timer.'"),
+        ]),
+        c("type TimerGroup struct", "A TimerGroup represents a group of timers relevant to a single query.", [
+          "timers map[fmt.Stringer]*Timer",
+          "child  *TimerGroup",
+          d("(t *TimerGroup) GetTimer(name fmt.Stringer) *Timer", "'GetTimer gets (and creates, if necessary) the Timer for a given code section.'"),
+        ]),
+        d("NewTimerGroup() *TimerGroup", "'NewTimerGroup constructs a new TimerGroup.'"),
+        c("type Timers []*Timer", "Timers is a slice of Timer pointers that implements Len and Swap from sort.Interface.", [
+          d("(t Timers) Len() int", "'Len implements sort.Interface.'"),
+          d("(t Timers) Swap(i, j int)", "'Swap implements sort.Interface.'"),
+          d("(t *TimerGroup) String() string", "'Return a string representation of a TimerGroup.'"),
+        ]),
+        c("type QueryTiming int", "QueryTiming identifies the code area or functionality in which time is spent during a query.", [
+          d("(s QueryTiming) String() string", "'Return a string represenation of a QueryTiming identifier.'"),
+        ]),
+
+        c("const", "Query timings.", [
+          "TotalEvalTime QueryTiming = iota",
+          "ResultSortTime",
+          "JSONEncodeTime",
+          "PreloadTime",
+          "TotalQueryPreparationTime",
+          "InnerViewBuildingTime",
+          "InnerEvalTime",
+          "ResultAppendTime",
+          "QueryAnalysisTime",
+          "GetValueAtTimeTime",
+          "GetBoundaryValuesTime",
+          "GetRangeValuesTime",
+          "ExecQueueTime",
+          "ViewDiskPreparationTime",
+          "ViewDataExtractionTime",
+          "ViewDiskExtractionTime",
+        ]),
+      ]),
+      d("strutil"),
+      d("testutil"),
+      c("treecache", [
+        c("type ZookeeperLogger struct", [
+          d("(zl ZookeeperLogger) Printf(s string, i ...interface{})", "Implements zk.Logger"),
+        ]),
+        c("type ZookeeperTreeCache struct", [
+          "conn     *zk.Conn",
+          "prefix   string",
+          "events   chan ZookeeperTreeCacheEvent",
+          "zkEvents chan zk.Event",
+          "stop     chan struct{}",
+          "head     *zookeeperTreeCacheNode",
+        ]),
+        c("type ZookeeperTreeCacheEvent struct", [
+          "Path string",
+          "Data *[]byte",
+          "(tc *ZookeeperTreeCache) Stop()",
+          "(tc *ZookeeperTreeCache) loop(failureMode bool)",
+          "(tc *ZookeeperTreeCache) recursiveNodeUpdate(path string, node *zookeeperTreeCacheNode) error",
+          "(tc *ZookeeperTreeCache) recursiveDelete(path string, node *zookeeperTreeCacheNode)",
+        ]),
+        "NewZookeeperTreeCache(conn *zk.Conn, path string, events chan ZookeeperTreeCacheEvent) *ZookeeperTreeCache",
+      ]),
+    ]),
+    c("version", [
+      c("var", "Build information. Populated at build-time.", [
+        "Version   string",
+        "Revision  string",
+        "Branch    string",
+        "BuildUser string",
+        "BuildDate string",
+        "GoVersion string",
+      ]),
+      c("var Map = map[string]string", "Map provides the iterable version information.", [
+        '"version":   Version',
+        '"revision":  Revision',
+        '"branch":    Branch',
+        '"buildUser": BuildUser',
+        '"buildDate": BuildDate',
+        '"goVersion": GoVersion',
+      ]),
+    ]),
+    d("web"),
+    c("web", [
+      c("api", [
+        d("legacy"),
+        d("v1"),
+      ]),
+      d("ui"),
+    ]),
   ]
 });
